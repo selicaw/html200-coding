c4ee0a4aaa7059f3ee9352897f6df218482472c9
Selica <wselica@gmail.com>
2020-07-16 04:06:28 -0700

    lesson 2 assignment js calculator

---
+++ lesson-02/calculator.js
@@ -0,0 +1,15 @@
+// Write a program called calculator.js.
+// Declare at least 4 variables, and then do math operations on them.
+// Mix and match operators and variables and console log the results of these operations.
+
+let a = 6;
+let b = 9;
+let c = 4;
+let d = 8;
+
+console.log (b + c * d);
+console.log (b % c);
+console.log (++a);
+console.log (a++);
+console.log (a++ - 1);
+console.log (b **5);
