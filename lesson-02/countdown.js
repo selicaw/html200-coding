f75e505e54848c075ed27fc1ea857becf430ce6a
Selica <wselica@gmail.com>
2020-07-19 20:56:32 -0700

    lesson 2 assignment js countdown

---
+++ lesson-02/countdown.js
@@ -0,0 +1,7 @@
+//Use one loop to console log a variable number.
+//Decrease it by one.
+//Log that number, and stop after the number prints 0.
+
+for (let x = 10; x >=0; x--) {
+  console.log(x);
+}
