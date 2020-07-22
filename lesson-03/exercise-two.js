00d0b4cf9b0d56272222212a951fa0368294a1cb
Selica <wselica@gmail.com>
2020-07-22 01:42:09 -0700

    lesson 3 assignment 2

---
+++ lesson-03/exercise-two.js
@@ -0,0 +1,9 @@
+/* Regular Challenge 2*/
+
+//Make a string variable of a sentence with at least five words and a period.
+//Make an array from that sentence and console.log the third word.
+
+let str = 'You came after me in your Herman Munster shoes.'
+let arr = str.split(' ');
+
+console.log(arr[2]);
