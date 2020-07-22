f660c01e50f620e23bf7ff01a94fbb6d3c62a6d1
Selica <wselica@gmail.com>
2020-07-22 02:36:34 -0700

    added exercise three

---
+++ lesson-03/exercise-three.js
@@ -0,0 +1,10 @@
+/* Regular Challenge 3 */
+
+// Create an array that has at least 5 strings:
+// Use a for loop to console log all of the elements plus the index and a comma
+
+let housewives=['Orange County', 'New York', 'Atlanta', 'New Jersey', 'Beverly Hills'];
+
+for (let i = 0; i <= housewives.length; i++) {
+  console.log(i + "," + housewives[i]);
+}
